fb6c34f6f0011143621ceca0c17ba6926133bc84
t37qfr <a.gabor.feher@gmail.com>
2018-05-12 18:49:04 +0100

    second

---
+++ patterns.py
@@ -0,0 +1,14 @@
+'''
+Type of design patterns:
+1. Creational
+ - Create objects in a systematic way
+ - Felxible
+ - Dominant OOP Mechanism: Polymorphism
+2. Structural
+ - Establish usuful relationships between componenets
+ - Dominant OOP Mechanism: Inheritence
+3. Behavioral
+ - Best practices of objects interaction
+ - Focus: Define protocols
+ - Dominant OOP Mechanism: Methods and their signitures
+'''
